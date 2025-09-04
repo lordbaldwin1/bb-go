0fb98e0a5aa47b7e4e7f02ce23b7058459450465
zach <springerczachary@gmail.com>
2025-09-03 21:41:12 -0700

    enpassant parsing, still ne clock stuffs

--- main.go
+++ main.go
@@ -162,6 +162,17 @@ var charPieces = map[byte]int{
 	'p': p, 'n': n, 'b': b, 'r': r, 'q': q, 'k': k,
 }
 
+var coordToSquare = map[string]int{
+	"a8": 0, "b8": 1, "c8": 2, "d8": 3, "e8": 4, "f8": 5, "g8": 6, "h8": 7,
+	"a7": 8, "b7": 9, "c7": 10, "d7": 11, "e7": 12, "f7": 13, "g7": 14, "h7": 15,
+	"a6": 16, "b6": 17, "c6": 18, "d6": 19, "e6": 20, "f6": 21, "g6": 22, "h6": 23,
+	"a5": 24, "b5": 25, "c5": 26, "d5": 27, "e5": 28, "f5": 29, "g5": 30, "h5": 31,
+	"a4": 32, "b4": 33, "c4": 34, "d4": 35, "e4": 36, "f4": 37, "g4": 38, "h4": 39,
+	"a3": 40, "b3": 41, "c3": 42, "d3": 43, "e3": 44, "f3": 45, "g3": 46, "h3": 47,
+	"a2": 48, "b2": 49, "c2": 50, "d2": 51, "e2": 52, "f2": 53, "g2": 54, "h2": 55,
+	"a1": 56, "b1": 57, "c1": 58, "d1": 59, "e1": 60, "f1": 61, "g1": 62, "h1": 63,
+}
+
 var SquareToBigInt = []uint64{
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
@@ -444,22 +455,24 @@ func parseFEN(fen string) {
 
 		switch c {
 		case 'K':
-			fmt.Println("white king")
 			castle |= WK
 		case 'Q':
-			fmt.Println("white queen")
+
 			castle |= WQ
 		case 'k':
-			fmt.Println("black king")
 			castle |= BK
 		case 'q':
-			fmt.Println("black queen")
+
 			castle |= BQ
 		default:
 			continue
 		}
 	}
 
+	// enpassant square
+	if splitFen[2] != "-" {
+		enpassant = coordToSquare[splitFen[2]]
+	}
 }
 
 func isAlpha(character byte) bool {
@@ -1183,7 +1196,7 @@ func initAll() {
 func main() {
 	initAll()
 
-	parseFEN(TRICKY_POSITION)
+	parseFEN(KILLER_POSITION)
 
 	printBoard()
 }
